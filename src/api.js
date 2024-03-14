2cddea0e9b35d8bd46924961ea2e8674e49800eb
KateCory <katecorywright@hotmail.com>
2024-03-14 11:13:53 +0000

    task 5 completed

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const ncNewsApi = axios.create({
+    baseURL: 'https://nc-news-api-1-0ko3.onrender.com/api'
+});
+
+export const fetchArticles = () => {
+    
+    return ncNewsApi.get('/articles').then(( { data: {articles} } ) => {
+        return articles; 
+    })
+};
+
+export const fetchArticleById = (article_id) => {
+    return ncNewsApi.get(`/articles/${article_id}`).then( ( { data: {article} } ) => {
+        return article;
+    })
+};
+
---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchArticleById } from "../api";
+
+const SingleArticle = () => {
+    const { article_id } = useParams();
+    const [singleArticle, setSingleArticle] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchArticleById(article_id).then((articleFromApi) => {
+            setSingleArticle(articleFromApi);
+            setIsLoading(false);
+        });
+    }, [article_id]);
+
+    return isLoading 
+    ? (<p>Now Loading</p>)
+    : (
+        <section id="single-article">
+            <h2>Article: {singleArticle.article_id}</h2>
+            <img src= {singleArticle.article_img_url} width='150' height='150'/>
+            <h3>{singleArticle.title}</h3>
+            <h4>Author: {singleArticle.author}</h4>
+            <p>Topic: {singleArticle.topic}</p> 
+            <p>{singleArticle.body}</p> 
+            <p>Votes: {singleArticle.votes}</p> 
+            <p>Date: {singleArticle.created_at}</p> 
+        </section>
+);
+};
+
+export default SingleArticle;
